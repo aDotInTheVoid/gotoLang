fc5665ccad2efc1ef9c71e857728f3942fb2e758
Nixon <nixon.emoony@gmail.com>
2018-07-08 13:36:13 +0100

    use gpl

--- asts.py
+++ asts.py
@@ -1,3 +1,21 @@
+# asts.py: defines Abstract Syntax Tree classes for gotoLang
+
+# Copyright 2017, 2018 Nixon Enraght-Moony
+
+# This file is part of gotoLang.
+
+# gotoLang is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 
+# as published by the Free Software Foundation
+
+# gotoLang is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with gotoLang.  If not, see <https://www.gnu.org/licenses/>.
+
 class AST(object):
     """Parent class for AST Nodes."""
 
